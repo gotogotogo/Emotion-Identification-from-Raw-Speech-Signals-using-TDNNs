de60760df3f398b2c0b082290dbc9bb5925a90a5
gotogotogo <1562850087@qq.com>
2021-06-03 14:35:20 +0800

    增加了速度扰动

--- dataset.py
+++ dataset.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
+import torch 
+from torch.utils.data import Dataset 
+import torchaudio  
+from torchaudio.transforms import Resample
+
 import numpy as np
 import pickle
-import torch
-from utils import utils_wav
-from torch.utils.data import Dataset
+from utils import truncate_wav
+
 
 class CustomDataset(Dataset):
     def __init__(self, wav_files_pkl, mode, test_sess):
@@ -13,7 +17,6 @@ class CustomDataset(Dataset):
         self.wav_paths = []
         self.labels = []
         self.gender_labels = []
-        self.data = []
         if self.mode == 'train':
             for session in data_dict:
                 if session[-1] != str(test_sess):
@@ -30,23 +33,33 @@ class CustomDataset(Dataset):
                         self.gender_labels.append(data_dict[session][wav_name]['gender'])
         else:
             assert False, 'Wrong mode!'
-        for wav_path in self.wav_paths:
-            extend_wav = utils_wav.load_wav(wav_path,min_dur_sec=8)
-            self.data.append(extend_wav)
-        with open(self.mode + '_data.pkl', 'wb') as f:
-            pickle.dump(self.data, f)
+        self.aug_data = []
+        self.aug_label = []
+        self.resample1 = Resample(16000, 16000 * 0.9)
+        self.resample2 = Resample(16000, 16000 * 1.0)
+        self.resample3 = Resample(16000, 16000 * 1.1)
+        for i in len(self.wav_paths):
+            waveform, sr = torchaudio.load(self.wav_paths[i])
+            self.aug_data.append(truncate_wav(self.resample1(waveform), sr, 8))
+            self.aug_label.append(self.labels[i])
+            self.aug_data.append(truncate_wav(self.resample2(waveform), sr, 8))
+            self.aug_label.append(self.labels[i])
+            self.aug_data.append(truncate_wav(self.resample3(waveform), sr, 8))
+            self.aug_label.append(self.labels[i])
         print(len(self.labels))
+        print(len(self.aug_label))
 
     def __len__(self):
         return len(self.labels)
     
     def __getitem__(self, index):
-        wav_path = self.wav_paths[index]
-        label = self.labels[index]
-        #gender = self.gender_labels[index]
-        extend_wav = utils_wav.load_wav(wav_path,min_dur_sec=8)
+        # wav_path = self.wav_paths[index]
+        # label = self.labels[index]
+        # #gender = self.gender_labels[index]
+        # extend_wav = utils_wav.load_wav(wav_path,min_dur_sec=8)
+        # sample = {'raw_speech': torch.from_numpy(np.ascontiguousarray(extend_wav)), 'labels': torch.from_numpy(np.ascontiguousarray(label))}
+        # return sample
+        extend_wav = self.aug_data[index]
+        label = self.aug_label[index]
         sample = {'raw_speech': torch.from_numpy(np.ascontiguousarray(extend_wav)), 'labels': torch.from_numpy(np.ascontiguousarray(label))}
         return sample
-
-def augment(waveform):
-    pass
--- utils/utils_wav.py
+++ utils/utils_wav.py
@@ -1,79 +1,19 @@
 # Third Party
 import torchaudio
 import numpy as np
-# ===============================================
-#       code from Arsha for loading data.
-# This code extract features for a give audio file
-# ===============================================
-def load_wav(audio_filepath, min_dur_sec=10):
-    audio_data, sr = torchaudio.load(audio_filepath)
-    len_file = audio_data.shape[1]
-    
-    if len_file <int(min_dur_sec * sr):
-        dummy=np.zeros((1,int(min_dur_sec*sr)-len_file))
-        extened_wav = np.concatenate((audio_data,dummy), axis = 1)
+
+
+def truncate_wav(waveform, sr, duration):
+    len_wav = waveform.shape[-1]
+    if len_wav <int(duration * sr):
+        dummy=np.zeros((1,int(duration * sr) - len_wav))
+        extened_wav = np.concatenate((waveform,dummy), axis = 1)
     else:
-        extened_wav = audio_data[:, :int(min_dur_sec * sr)]
+        extened_wav = waveform[:, :int(duration * sr)]
     return extened_wav
 
-
-# def lin_mel_from_wav(wav, hop_length, win_length, n_mels):
-#     linear = librosa.feature.melspectrogram(wav, n_mels=n_mels, win_length=win_length, hop_length=hop_length) # linear spectrogram
-#     return linear.T
-
-# def lin_spectogram_from_wav(wav, hop_length, win_length, n_fft=512):
-#     linear = librosa.stft(wav, n_fft=n_fft, win_length=win_length, hop_length=hop_length) # linear spectrogram
-#     return linear.T
-
-
-# def feature_extraction(filepath,sr=16000, min_dur_sec=4,win_length=400,hop_length=160, n_mels=40, spec_len=400,mode='train'):
-#     audio_data = load_wav(filepath, sr=sr,min_dur_sec=min_dur_sec)
-#     linear_spect = lin_spectogram_from_wav(audio_data, hop_length, win_length, n_fft=512)
-#     mag, _ = librosa.magphase(linear_spect)  # magnitude
-#     mag_T = mag.T
-#     mu = np.mean(mag_T, 0, keepdims=True)
-#     std = np.std(mag_T, 0, keepdims=True)
-#     return (mag_T - mu) / (std + 1e-5)
-    
-    
-
-# def load_data_wav(filepath,sr=16000,min_dur_sec=10):
-#     extend_wav = load_wav(filepath, min_dur_sec=10)
-#     extend_wav = extend_wav.reshape(1, -1)
-#     return extend_wav
-    
-# def load_data(filepath,sr=16000, min_dur_sec=10,win_length=400,hop_length=160, n_mels=40, spec_len=1000,mode='train'):
-#     audio_data = load_wav(filepath, sr=sr,min_dur_sec=min_dur_sec)
-#     #linear_spect = lin_spectogram_from_wav(audio_data, hop_length, win_length, n_mels)
-#     linear_spect = lin_spectogram_from_wav(audio_data, hop_length, win_length, n_fft=512)
-#     mag, _ = librosa.magphase(linear_spect)  # magnitude
-#     mag_T = mag.T
-    
-#     if mode=='train':
-#         randtime = np.random.randint(0, mag_T.shape[1]-spec_len)
-#         spec_mag = mag_T[:, randtime:randtime+spec_len]
-#     else:
-#         spec_mag = mag_T
-    
-#     # preprocessing, subtract mean, divided by time-wise var
-#     mu = np.mean(spec_mag, 0, keepdims=True)
-#     std = np.std(spec_mag, 0, keepdims=True)
-#     return (spec_mag - mu) / (std + 1e-5)
-    
-
-
-# def load_npy_data(filepath,spec_len=1000,mode='train'):
-#     mag_T = np.load(filepath)
-#     if mode=='train':
-#         randtime = np.random.randint(0, mag_T.shape[1]-spec_len)
-#         spec_mag = mag_T[:, randtime:randtime+spec_len]
-#     else:
-#         spec_mag = mag_T
-#     return spec_mag
-    
-    
-
-
+def speed_perturbation():
+    pass 
 
 def speech_collate(batch):
     targets = []
