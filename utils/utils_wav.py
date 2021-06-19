70f88fb0aecd1a8d7f0964ee2475fe671f61ec2d
gotogotogo <1562850087@qq.com>
2021-06-19 14:52:32 +0800

    v2 修改了数据增强的方式。原始的方式是将增强后的增大了三十倍的所有数据作为一个epoch的数据，修改后每个epoch实施随机的增强，数据量没有增多

--- utils/utils_wav.py
+++ utils/utils_wav.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torchaudio
+from torchaudio.transforms import Resample
 import numpy as np
 import random
 
@@ -9,38 +10,48 @@ def get_random_index(max_num, length):
     indexes.sort()
     return indexes
 
-def truncate_wav(waveform, sr, duration):
-    len_wav = waveform.shape[-1]
-    dur = len_wav / sr
+def truncate_wav(waveform, duration, sr=16000):
+    '''
+        truncate waveform to the specified time duration 
+        return :
+            extend_wav: truncated waveform
+    '''
+    cur_len = waveform.shape[-1]
     standard_len = int(duration * sr)
-    # if len_wav < standard_len:
-    #     times = standard_len // len_wav
-    #     waveform = waveform.repeat(1, times)
-    #     len_wav = len_wav * times
-    #     dummy=np.zeros((1,standard_len - len_wav))
-    #     extend_wav = np.concatenate((waveform,dummy), axis = 1)
-    if len_wav < standard_len:
-        dummy = np.zeros((1, standard_len - len_wav))
+    if cur_len < standard_len:
+        dummy = np.zeros((1, standard_len - cur_len))
         extend_wav = np.concatenate((waveform, dummy), axis = 1)
-    elif len_wav > standard_len:
+    elif cur_len > standard_len:
         # extend_wav = waveform[:, :int(duration * sr)]
-        indexes = get_random_index(len_wav, standard_len)
+        indexes = get_random_index(cur_len, standard_len)
         extend_wav = waveform[:, indexes]
     else:
         extend_wav = waveform
 
-    return extend_wav, dur
+    return extend_wav
 
-def speed_perturbation():
-    pass 
+def resample(waveform):
+    rate = 1 + (random.randint(0, 2) - 1) * 0.1
+    resample = Resample(16000, 16000 * rate)
+    return resample(waveform) 
+
+def amplitude_modulate(waveform, min_gain_db=-12, max_gain_db=12):
+    amplitude = random.uniform(min_gain_db, max_gain_db)
+    amplitude = 10 ** (amplitude / 20)
+    return waveform * amplitude
+
+def augment(waveform, duration):
+    extend_wav = resample(waveform)
+    extend_wav = amplitude_modulate(extend_wav)
+    extend_wav = truncate(extend_wav, duration)
+    return extend_wav
 
 def speech_collate(batch):
     targets = []
     raw_data = []
     duration = []
     for sample in batch:
-        raw_data.append(sample['raw_speech'])
-        targets.append(sample['labels'])
+        raw_data.append(sample['waveform'])
+        targets.append(sample['label'])
         duration.append(sample['duration'])
     return raw_data, targets, duration
-
