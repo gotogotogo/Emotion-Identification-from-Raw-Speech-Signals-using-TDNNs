c735752fbe3b9f766c6b47270ee454b0bddb4ecb
linhuajian <1562850087@qq.com>
2021-07-03 20:52:46 +0800

    v6

--- training_Emo_TDNN_StatPool.py
+++ training_Emo_TDNN_StatPool.py
@@ -31,7 +31,7 @@ parser.add_argument('--raw_wav_path', type=str, default='raw_wavs.pkl')
 
 parser.add_argument('-input_dim', action="store_true", default=1)
 parser.add_argument('-num_classes', action="store_true", default=4)
-parser.add_argument('-batch_size', action="store_true", default=64)
+parser.add_argument('--batch_size', action="store_true", default=64)
 parser.add_argument('-use_gpu', action="store_true", default=True)
 parser.add_argument('-num_epochs', action="store_true", default=1000)
 parser.add_argument('--lr', type=float, default=0.0001)
@@ -54,10 +54,10 @@ class Cross_Entropy_Loss_Label_Smooth(nn.Module):
 
 ### Data related
 dataset_train = CustomDataset(args.raw_wav_path, mode='train', test_sess=5, duration=2)
-dataloader_train = DataLoader(dataset_train, batch_size=args.batch_size, collate_fn=speech_collate, shuffle=True, drop_last=True, num_workers=8, pin_memory=True)  
+dataloader_train = DataLoader(dataset_train, batch_size=args.batch_size, collate_fn=speech_collate, shuffle=True, drop_last=True, num_workers=14, pin_memory=True)  
 
 dataset_test = CustomDataset(args.raw_wav_path, mode='test', test_sess=5, duration=2)
-dataloader_test = DataLoader(dataset_test, batch_size=args.batch_size, collate_fn=speech_collate, shuffle=False, drop_last=True, num_workers=8, pin_memory=True)  
+dataloader_test = DataLoader(dataset_test, batch_size=args.batch_size, collate_fn=speech_collate, shuffle=False, drop_last=True, num_workers=14, pin_memory=True)  
 
 ## Model related
 use_cuda = torch.cuda.is_available()
