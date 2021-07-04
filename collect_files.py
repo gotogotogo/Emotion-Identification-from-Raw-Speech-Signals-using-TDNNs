22d3a1d706c70af1f994fa6f38d6acd9ce2337a5
linhuajian <1562850087@qq.com>
2021-07-04 15:21:59 +0800

    读取元数据中的vad

--- collect_files.py
+++ collect_files.py
@@ -7,6 +7,7 @@ import glob
 import pickle
 import argparse
 from tqdm import tqdm
+from utils.utils_wav import resample
 import torchaudio
 
 parser = argparse.ArgumentParser(add_help=False)
@@ -16,9 +17,9 @@ gender_id = {'M':0,'F':1}
 def collect_files(root_path):
     '''
     data_dict:{
-        'Session1': [{'wav': wav, 'emotion': emotion, 'gender': gender, 'duration': duration},...],
+        'Session1': [{'wav': wav, 'emotion': emotion, 'gender': gender, 'duration': duration, 'vad': [V, A, D]},...],
         ......
-        'Session5': [{'wav': wav, 'emotion': emotion, 'gender': gender, 'duration': duration},...],
+        'Session5': [{'wav': wav, 'emotion': emotion, 'gender': gender, 'duration': duration, 'vad': [V, A, D]},...],
     }
     '''
     data_dict = {}
@@ -28,6 +29,7 @@ def collect_files(root_path):
         emo_dir = os.path.join(root_path, speaker, 'dialog/EmoEvaluation')
         for sess in os.listdir(wav_dir):
             label_list = {}
+            VAD_list = {}
             emotxt = emo_dir + '/' + sess + '.txt'
             with open(emotxt, 'r') as f:
                 while True:
@@ -36,7 +38,11 @@ def collect_files(root_path):
                         break
                     if(line[0] == '['):
                         t = line.split()
+                        V = t[5][1:-1]
+                        A = t[6][0:-1]
+                        D = t[7][: -1]
                         label_list[t[3]] = t[4]
+                        VAD_list[t[3]] = [float(V), float(A), float(D)]
             files = glob.glob(os.path.join(wav_dir, sess, '*.wav'))
             for file_name in files:
                 #print(file_name)
@@ -44,8 +50,28 @@ def collect_files(root_path):
                 waveform, _ = torchaudio.load(file_name)
                 #print(wave_name)
                 emotion = label_list[wave_name]
+                VAD = VAD_list[wave_name]
                 if emotion in ['ang', 'sad', 'neu', 'exc', 'hap']:
-                    data_dict[speaker].append({'wav': waveform, 'emotion': emotion_id[emotion], 'gender': gender_id[wave_name[5]], 'duration': waveform.shape[1] / 16000})
+                    if speaker[-1] != '5':
+                        for rate in [0.9, 1.0, 1.1]:
+                            extend_wav = resample(waveform, rate)
+                            data_dict[speaker].append(
+                            {
+                                'wav': extend_wav,
+                                'emotion': emotion_id[emotion],
+                                'gender': gender_id[wave_name[5]],
+                                'duration': extend_wav.shape[1] / 16000,
+                                'vad': VAD
+                            })
+                    else:
+                        data_dict[speaker].append(
+                            {
+                                'wav': waveform,
+                                'emotion': emotion_id[emotion],
+                                'gender': gender_id[wave_name[5]],
+                                'duration': waveform.shape[1] / 16000,
+                                'vad': VAD
+                            })
         print('len of ', speaker, ' :', len(data_dict[speaker]))
     with open('raw_wavs.pkl', 'wb') as f:
         pickle.dump(data_dict, f)
