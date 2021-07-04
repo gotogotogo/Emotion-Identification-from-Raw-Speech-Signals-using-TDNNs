ed674fe437a1e557876a102f6d909316b642c96d
gotogotogo <1562850087@qq.com>
2021-07-04 13:57:28 +0800

    读取源文件中的vad值

--- collect_files.py
+++ collect_files.py
@@ -16,9 +16,9 @@ gender_id = {'M':0,'F':1}
 def collect_files(root_path):
     '''
     data_dict:{
-        'Session1': [{'wav': wav, 'emotion': emotion, 'gender': gender, 'duration': duration},...],
+        'Session1': [{'wav': wav, 'emotion': emotion, 'gender': gender, 'duration': duration, 'vad': [V, A, D]},...],
         ......
-        'Session5': [{'wav': wav, 'emotion': emotion, 'gender': gender, 'duration': duration},...],
+        'Session5': [{'wav': wav, 'emotion': emotion, 'gender': gender, 'duration': duration, 'vad': [V, A, D]},...],
     }
     '''
     data_dict = {}
@@ -28,6 +28,7 @@ def collect_files(root_path):
         emo_dir = os.path.join(root_path, speaker, 'dialog/EmoEvaluation')
         for sess in os.listdir(wav_dir):
             label_list = {}
+            VAD_list = {}
             emotxt = emo_dir + '/' + sess + '.txt'
             with open(emotxt, 'r') as f:
                 while True:
@@ -36,16 +37,21 @@ def collect_files(root_path):
                         break
                     if(line[0] == '['):
                         t = line.split()
+                        V = t[5][1:-1]
+                        A = t[6][0:-1]
+                        D = t[7][:-1]
                         label_list[t[3]] = t[4]
+                        VAD_list[t[3]] = [float(V), float(A), float(D)]
             files = glob.glob(os.path.join(wav_dir, sess, '*.wav'))
             for file_name in files:
                 #print(file_name)
-                wave_name = file_name.split('/')[-1][:-4]
+                wave_name = file_name.split('\\')[-1][:-4]
                 waveform, _ = torchaudio.load(file_name)
                 #print(wave_name)
                 emotion = label_list[wave_name]
+                VAD = VAD_list[wave_name]
                 if emotion in ['ang', 'sad', 'neu', 'exc', 'hap']:
-                    data_dict[speaker].append({'wav': waveform, 'emotion': emotion_id[emotion], 'gender': gender_id[wave_name[5]], 'duration': waveform.shape[1] / 16000})
+                    data_dict[speaker].append({'wav': waveform, 'emotion': emotion_id[emotion], 'gender': gender_id[wave_name[5]], 'duration': waveform.shape[1] / 16000, 'vad': VAD})
         print('len of ', speaker, ' :', len(data_dict[speaker]))
     with open('raw_wavs.pkl', 'wb') as f:
         pickle.dump(data_dict, f)
